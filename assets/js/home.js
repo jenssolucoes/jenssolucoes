ac4b497d77521568aba23543122fe422df0e912b
jenssolucoes <jenssolucoes@gmail.com>
2026-09-18 17:49:44 -0300

    g

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,26 +10,38 @@ class HomePage {
     }
 
     init() {
-        // Inicializa o Swiper para os depoimentos
-        if (typeof Swiper !== 'undefined') {
-            new Swiper('.reviews-swiper', {
-                slidesPerView: 1,
-                spaceBetween: 30,
-                loop: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
+        this.initReviewsSwiper();
+    }
+
+    initReviewsSwiper() {
+        const el = document.querySelector('.reviews-swiper');
+        if (!el || typeof Swiper === 'undefined') return;
+
+        new Swiper(el, {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            grabCursor: true,
+            watchOverflow: true,
+            speed: 900,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
+            pagination: {
+                el: el.querySelector('.swiper-pagination'),
+                clickable: true,
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
                 },
-                breakpoints: {
-                    768: {
-                        slidesPerView: 2,
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                    }
+                1024: {
+                    slidesPerView: 3,
                 }
-            });
-        }
+            }
+        });
     }
 }
 
